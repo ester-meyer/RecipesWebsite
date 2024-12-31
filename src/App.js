560229eab3ff48b93107e66aaf4e2e1a894699f7
ester-meyer <mindy05832@gmail.com>
2024-12-31 20:50:02 +0200

    partial upDate for RecipeCard

--- src/App.js
+++ src/App.js
@@ -12,8 +12,8 @@ function App() {
     <>
       <Navbar/>
       {/* <SignUp/> */}
-      {/* <RecipeCard id={n}/> */}
-      <RecipeList/>
+      <RecipeCard id={n}/>
+      {/* <RecipeList/> */}
       {/* <Home/> */}
     </>
   );
